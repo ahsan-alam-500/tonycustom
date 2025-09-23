add95f841a5289ddb507fce24c5d750ba97a19f3
Ahsan Alam <ahsansfiverr@gmail.com>
2025-09-23 12:13:01 +0600

    card customizer component and relation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -22,4 +22,57 @@ class Product extends Model
         return $this->belongsTo(Category::class);
     }
 
+    public function images()
+    {
+        return $this->hasMany(ProductHasImage::class);
+    }
+
+    public function skin_tones()
+    {
+        return $this->hasMany(SkinTone::class);
+    }
+
+    public function hairs()
+    {
+        return $this->hasMany(Hair::class);
+    }
+
+    public function noses()
+    {
+        return $this->hasMany(Nose::class);
+    }
+
+    public function eyes()
+    {
+        return $this->hasMany(Eye::class);
+    }
+
+    public function mouths()
+    {
+        return $this->hasMany(Mouth::class);
+    }
+
+    public function dresses()
+    {
+        return $this->hasMany(Dress::class);
+    }
+
+    public function crowns()
+    {
+        return $this->hasMany(Crown::class);
+    }
+
+    public function base_cards()
+    {
+        return $this->hasMany(BaseCard::class);
+    }
+
+    public function beards()
+    {
+        return $this->hasMany(Beard::class);
+    }
+
+
+
+
 }
