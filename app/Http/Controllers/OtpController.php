1a722c3f24d0917fb407ba1c6a6c87ca18cc8261
Ahsan Alam <ahsansfiverr@gmail.com>
2025-09-24 15:11:13 +0600

    updated table

--- app/Http/Controllers/OtpController.php
+++ app/Http/Controllers/OtpController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Mail\OtpMail;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -16,7 +17,18 @@ class OtpController extends Controller
 
         $otp = rand(1000, 9999);
 
+        $user = User::where('email', $request->email)->first();
 
+        if ($user) {
+            $user->otp = $otp;
+            $user->save();
+        }else {
+            return response()->json([
+                'success' => false,
+                'status' => 404,
+                'message' => 'User not found',
+            ]);
+        }
 
         // Send OTP via Email
         Mail::to($request->email)->send(new OtpMail($otp));
