cda11e686a92b4cfaafbda2cc72c563cdbb1abbb
Ahsan Alam <ahsansfiverr@gmail.com>
2025-09-25 14:49:50 +0600

    Order and order items models and migration

---
+++ app/Models/Order.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'name', 'email', 'phone', 'address', 'total', 'status', 'is_paid', 'is_customized', 'customized_file'
+    ];
+
+    protected $casts = [
+        'is_paid' => 'boolean',
+        'is_customized' => 'boolean',
+    ];
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
