e39a83ca287cc7c3c588835582d40ca78543f5e0
Ahsan Alam <ahsansfiverr@gmail.com>
2025-09-26 11:22:40 +0600

    Product

--- routes/api.php
+++ routes/api.php
@@ -68,3 +68,4 @@ Route::delete('contacts/{id}', [ContactController::class, 'destroy']);
 //=============================================================
 
 Route::post('contact', [ContactController::class, 'store']);
+Route::get('shop', [ProductController::class, 'index']);
