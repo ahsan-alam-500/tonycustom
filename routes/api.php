efaffe4d211c86959403dedf6f69557b0ea12f16
Ahsan Alam <ahsansfiverr@gmail.com>
2025-09-29 13:01:26 +0600

    Product controller update

--- routes/api.php
+++ routes/api.php
@@ -67,4 +67,4 @@ Route::delete('contacts/{id}', [ContactController::class, 'destroy']);
 
 Route::post('contact', [ContactController::class, 'store']);
 Route::get('shop', [ProductController::class, 'index']);
-Route::get('shop/{slug}', [ProductController::class, 'view']);
+Route::get('shop/{slug}', [ProductController::class, 'show']);
