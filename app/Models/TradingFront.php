7b9fa75dd04f30b5cad26f0431f39e910891f062
Ahsan Alam <ahsansfiverr@gmail.com>
2025-10-01 10:31:16 +0600

    new db table creation

---
+++ app/Models/TradingFront.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TradingFront extends Model
+{
+    protected $table = 'trading_fronts';
+    protected $fillable=[
+        'image',
+        'product_id'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
